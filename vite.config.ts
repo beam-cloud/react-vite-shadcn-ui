3c70ef5d2c4dbb52ac7b1525e5f7f56d5684c606
Luke Lombardi <luke@beam.cloud>
2025-06-30 15:34:18 -0400

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,13 @@ export default defineConfig({
     },
   },
   server: {
+    host: '::',
     port: 3000,
-    host: true,
-    hmr: {
-    },
-  },
+    strictPort: true,
+    hmr: false, // Disable HMR to prevent constant refresh
+    watch: {
+      usePolling: true, // Use polling instead of file system events
+      interval: 1000, // Poll every second
+    }
+  }
 });
