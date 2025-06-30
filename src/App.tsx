aa5b8d9bfe4bee5f28ba19980d86f54b5761b2a8
Luke Lombardi <luke@beam.cloud>
2025-06-30 11:21:21 -0400

    improve default styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import { Badge } from "@/components/ui/badge";
 
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col items-center gap-y-4">
+    <main className="flex flex-col items-center justify-center h-full w-full min-h-0">
+      <div className="flex flex-col items-center gap-y-4 p-4">
         <div className="inline-flex items-center gap-x-4">
-          <img src={ReactSVG} alt="React Logo" className="w-32" />
-          <span className="text-6xl">+</span>
-          <img src={"/vite.svg"} alt="Vite Logo" className="w-32" />
+          <img src={ReactSVG} alt="React Logo" className="w-24 md:w-32" />
+          <span className="text-4xl md:text-6xl">+</span>
+          <img src={"/vite.svg"} alt="Vite Logo" className="w-24 md:w-32" />
         </div>
         <a
           href="https://ui.shadcn.com"
