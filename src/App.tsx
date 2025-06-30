9220fc17a35471e7b3aa4ab1860998740233f87c
Luke Lombardi <luke@beam.cloud>
2025-06-30 16:17:29 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen w-screen min-h-0">
+    <main className="h-full w-full bg-background text-foreground p-4 overflow-auto">
       <div className="flex flex-col items-center gap-y-4 p-4">
         <div className="inline-flex items-center gap-x-4">
           <span className="text-4xl md:text-6xl">+</span>
