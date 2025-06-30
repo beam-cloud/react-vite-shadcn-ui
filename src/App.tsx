22149da5efb3851e35e6824a91d90b0319618db3
Luke Lombardi <luke@beam.cloud>
2025-06-30 17:28:39 -0400

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 function App() {
   return (
-    <main className="h-full w-full bg-background text-foreground p-4 overflow-auto">
-      <div className="flex flex-col items-center gap-y-4 p-4">
-        <div className="inline-flex items-center gap-x-4">
-          <span className="text-4xl md:text-6xl">+</span>
-        </div>
-      </div>
-    </main>
+    <main className="h-full w-full bg-background text-foreground overflow-auto"></main>
   );
 }
 
