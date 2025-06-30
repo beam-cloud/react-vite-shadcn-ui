aa3b773550bcd440c55bb40bb2907646306d6897
Luke Lombardi <luke@beam.cloud>
2025-06-30 15:56:38 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import CountBtn from "@/components/count-btn";
-import ReactSVG from "@/assets/react.svg";
-import { Badge } from "@/components/ui/badge";
-
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-full w-full min-h-0">
+    <main className="flex flex-col items-center justify-center h-screen w-screen min-h-0">
       <div className="flex flex-col items-center gap-y-4 p-4">
         <div className="inline-flex items-center gap-x-4">
-          <img src={ReactSVG} alt="React Logo" className="w-24 md:w-32" />
           <span className="text-4xl md:text-6xl">+</span>
-          <img src={"/vite.svg"} alt="Vite Logo" className="w-24 md:w-32" />
         </div>
-        <a
-          href="https://ui.shadcn.com"
-          rel="noopener noreferrer nofollow"
-          target="_blank"
-        >
-          <Badge variant="outline">shadcn/ui</Badge>
-        </a>
-        <CountBtn />
       </div>
     </main>
   );
